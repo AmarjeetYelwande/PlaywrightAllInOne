1cffccbe793f700d203178e768d0dc3308d9aec5
Amarjeet Yelwande <26715498+AmarjeetYelwande@users.noreply.github.com>
2026-09-16 10:27:22 +0100

    First Commmit

---
+++ API.Automation/Pages/TicketBookingHomePage.cs
@@ -0,0 +1,32 @@
+﻿using Automation.Framework.Hooks;
+using Microsoft.Playwright;
+using Shouldly;
+
+namespace API.Automation.Pages
+{
+    public class TicketBookingHomePage(Hooks hooks)
+    {
+        private readonly IPage _user = hooks.User;
+
+        // Locator for event links (assuming events are rendered as <ul><li><a> elements)
+        private ILocator EventLinks
+            => _user.Locator("ul li a");
+
+        public async Task AssertPageContent()
+        {
+            // Assert that the correct URL has been reached
+            _user.Url.ShouldBe("http://localhost:5140/");
+            
+            // Assert that at least one event link is visible
+            var eventCount = await EventLinks.CountAsync();
+            eventCount.ShouldBeGreaterThan(0, "No events were found on the home page.");
+        }
+
+        // Optional: click on an event by its id (if links include the event id)
+        public async Task ClickEventById(int eventId)
+        {
+            var eventLink = _user.Locator($"a[href='/eventdetails/{eventId}']");
+            await eventLink.ClickAsync();
+        }
+    }
+}
