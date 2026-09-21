97860738b70c42fe5cf943eef71e41d30c80ada9
amarjeet.yelwande2 <amarjeet.yelwande2@kainos.com>
2026-09-21 11:23:35 +0100

    Remove unsed usings

--- TicketBookingApp/Program.cs
+++ TicketBookingApp/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using TicketBookingApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
